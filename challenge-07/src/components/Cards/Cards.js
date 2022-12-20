5040ed71b218f9cbfea0c8f98fd2ad6b6542bf4d
dila-rn <dila.rn24@gmail.com>
2022-12-20 23:58:46 +0700

    add capacity filter

--- challenge-07/src/components/Cards/Cards.js
+++ challenge-07/src/components/Cards/Cards.js
@@ -21,7 +21,36 @@ const Cards = () => {
     setCars(cars.data);
   };
 
-  
+  const onFilterCarCapacityParam = async (event) => {
+    const capacityParam = parseInt(event.target.value);
+
+    if (capacityParam === null || capacityParam === "") {
+        getAllCars();
+    }
+
+    setFilterCar((prevState) => ({
+        ...prevState,
+        capacity: capacityParam
+    }))
+    console.log(capacityParam);
+  };
+
+  const onShowFilterCars = async (filter) => {
+    if (filter.capacity != null) {
+        const filteredCars = await axios.get(CARS_ENDPOINT_URL + '?capacity=' + filter.capacity);
+        setCars(filteredCars.data);
+        console.log(filteredCars.data)
+    };
+  }
+
+  const onEventSubmitFilter = (event) => {
+
+    onShowFilterCars(filterCarParams);
+    console.log(filterCarParams);
+
+    event.preventDefault();
+};
+
 
   return (
     <div>
@@ -29,7 +58,7 @@ const Cards = () => {
         <div className=''>
           <div className="d-flex justify-content-center p-5">
             <div className="search-rectangle p-4" style={{ position: "relative", marginTop: "-3%" }}>
-              <form className="row p-2" >
+              <form className="row p-2" onSubmit={(event) => onEventSubmitFilter(event)}>
                 <div className="col p-2">
                   <label className="form-label">Tipe Driver</label>
                   <select className="form-select" id="tipe-driver">
@@ -41,11 +70,11 @@ const Cards = () => {
                 <div className="col p-2">
                   <label className="form-label">Tanggal</label>
                   <input className="form-control" id="tanggal-booking" type="date"
-                    placeholder="Pilih Tanggal"  />
+                    placeholder="Pilih Tanggal" />
                 </div>
                 <div className="col p-2">
                   <label className="form-label">Waktu Jemput/Ambil</label>
-                  <select id="waktu-booking" className="form-select" >
+                  <select id="waktu-booking" className="form-select">
                     <option defaultValue value="default">Pilih waktu jemput</option>
                     <option value="08:00">08.00 WIB</option>
                     <option value="09:00">09.00 WIB</option>
@@ -56,7 +85,7 @@ const Cards = () => {
                 </div>
                 <div className="col-3 p-2">
                   <label className="form-label">Jumlah Penumpang (optional)</label>
-                  <select id="jumlah-penumpang" className="form-select">
+                  <select id="jumlah-penumpang" className="form-select" onChange={(event) => onFilterCarCapacityParam(event)}>
                     <option defaultValue value="0">Pilih Jumlah Penumpang</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -67,7 +96,7 @@ const Cards = () => {
                   </select>
                 </div>
                 <div className="col-2 p-2 d-flex align-items-end">
-                  <button type="submit" className={styles.Button} id="submit-btn" >Cari Mobil</button>
+                  <button type="submit" className={styles.Button} id="submit-btn">Cari Mobil</button>
                 </div>
               </form>
             </div>
